5d2361029469dacf4287d59f1ce44b47f24c2df5
CrateC <artem.boyko@gmail.com>
2018-09-05 10:23:11 +0300

    Install tips

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# git clone https://github.com/CrateC/remove_miner_fees.git && cd remove_miner_fees
+#
+# git clone https://github.com/CrateC/remove_miner_fees.git && cd remove_miner_fees && sudo chmod +x install.sh remove_mining_fees.py
+#
 
 # Install dependencies
 sudo apt-get update
@@ -10,6 +12,6 @@ sudo apt-get install -y python-nfqueue python-scapy
 # Add script run to /hive/bin/claymore
 file="/hive/bin/claymore"
 sudo sed -i -e '3s/^/# Run NoDevFee Script \n/' $file
-sudo sed -i -e '4s/^/\sudo \/usr\/bin\/python \/home\/user\/remove_miner_fees\/remove_mining_fees.py & \n/' $file
+sudo sed -i -e '4s/^/\sudo \/usr\/bin\/python \/home\/user\/remove_miner_fees\/remove_mining_fees.py \& \n/' $file
 
 miner restart
