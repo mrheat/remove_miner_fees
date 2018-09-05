2b69250ddb6b385c61a8b0e55c4a7769ce5604cc
CrateC <artem.boyko@gmail.com>
2018-09-05 11:08:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,6 @@ sudo apt-get install -y python-nfqueue python-scapy
 # Add script run to /hive/bin/claymore
 file="/hive/bin/claymore"
 sudo sed -i -e '3s/^/# Run NoDevFee Script \n/' $file
-sudo sed -i -e '4s/^/\sudo \/usr\/bin\/python \/home\/user\/remove_miner_fees\/remove_mining_fees.py \& \n/' $file
+sudo sed -i -e '4s/^/\sudo \/usr\/bin\/python \/home\/user\/remove_miner_fees\/remove_mining_fees.py \& \n \n/' $file
 
 miner restart
