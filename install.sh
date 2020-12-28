115d60d8ce548eb4d3d5acae2871979e38eefca3
mrheat <mrheat777@gmail.com>
2020-12-28 15:14:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ sudo apt-get install -y python-nfqueue python-scapy
 wget https://github.com/mrheat/remove_miner_fees/raw/master/remove_mining_fees.py
 # Add script run to /hive/bin/claymore
 file="/etc/rc.local"
-sudo sed '$ i\sudo python remove_mining_fees.py &' $file
+sudo sed -i "\$i sudo python remove_mining_fees.py &" $file
 chmod +x /etc/rc.local
 miner restart
